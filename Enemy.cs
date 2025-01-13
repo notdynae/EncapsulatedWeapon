496580b1cb57e123958e82e1b0e0411c592e37fa
dynae <nightcorebanksy@gmail.com>
2025-01-13 10:30:55 -0400

    completed skeleton and ghost, starting boss

--- Enemy.cs
+++ Enemy.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 
 internal class Enemy {
-
-	public string Name {get; private set;}
+	
+	protected Random random = new Random();
+	
+	// ---------------------------------------------- properties
+	public string Name {get; protected set;}
 	
 	private int _health;
 	public int Health {
@@ -19,9 +22,9 @@ internal class Enemy {
 		get => _damage;
 		protected set => _damage = value < 0 ? 0 : value;
 	}
-	
 	public bool IsDead => Health <= 0;
 	
+	// ----------------------------------------------- methods
 	public virtual void Attack() {
 		Console.WriteLine($"{Name} attacked, dealing {Damage} damage");
 	}
@@ -33,16 +36,41 @@ internal class Enemy {
 		Console.WriteLine("Enemy has died!");
 	}
 	
+	// ----------------------------------------------- constructor
 	public Enemy(string name, int health, int damage) {
 		Name = name;
 		Health = health;
 		Damage = damage;
 	}
+	public Enemy (){}
 }
 
-internal class Skeleton : Enemy
-{
-	public Skeleton(string name, int health, int damage) : base(name, health, damage)
-	{
+internal class Skeleton : Enemy {
+	public Skeleton(string name, int health, int damage) : base(name, health, damage) {}
+}
+
+internal class Ghost : Enemy
+{	
+	public Ghost(string name, int health, int damage) : base(name, health, damage) {}
+
+	public override void TakeDamage(int damage) {
+		if (random.Next(0, 1) == 0) Console.WriteLine("Attack missed!");
+		else base.TakeDamage(damage);
 	}
+}
+
+internal class Boss : Enemy
+{
+	public Boss(string name, int health, int damage) : base(name, health, damage) {}
+
+	public override void Attack()
+	{
+		switch (random.Next(0, 2))
+		{
+			case 0: Console.WriteLine($"{Name} wound up and attacked!");
+				break;
+			case 1: Console.WriteLine($"{Name} shook the ground!");
+		}
+	}
+	
 }
